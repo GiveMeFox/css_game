a9328ac97a0d82c96c510c791495d56ec3dcab58
bogusz <bogusz2005@gmail.com>
2024-10-30 09:46:24 +0100

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { DarkMode } from 'flowbite-svelte';
+	let btnClass = 'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2';
+
 	let { children } = $props();
 </script>
 
+<span class="sticky top-0 flex justify-between items-center p-3">
+	<span></span>
+	<DarkMode {btnClass} />
+</span>
 {@render children()}
