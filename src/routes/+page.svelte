078b566cb2c7eacf501707ba5cf837f0881e37f4
bogusz <bogusz2005@gmail.com>
2024-10-30 11:07:15 +0100

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,25 @@
 <script>
 
 
-	let card = "w-[50%] h-[100%] bg-gray-200 m-1 border-2 border-current rounded-md flex flex-col";
+	let card = 'w-[100%] h-[50%] bg-gray-200 border-2 border-current rounded-md';
 </script>
 
-<span class="flex w-[100%] h-[90%]">
-	<span class={card}>
-		<span class="h-[50%]" >
+<span class="flex w-[100%] h-[90%] gap-1 p-1">
+	<span class="flex flex-col w-[50%] h-[100%] gap-1">
+			<span class={card}>
 
+			</span>
+			<span class={card}>
 
-		</span>
-		<span class="h-[50%]" >
-
-
-		</span>
+			</span>
 	</span>
-	<span class={card}>
-		<span class="h-[50%]" >
 
+	<span class="flex flex-col w-[50%] h-[100%] gap-1">
+			<span class={card}>
 
-		</span>
-		<span class="h-[50%]" >
+			</span>
+			<span class={card}>
 
-
-		</span>
-
+			</span>
 	</span>
 </span>
